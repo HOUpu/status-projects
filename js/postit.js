2be12ef6952e2cc5322defda9397f410406c417a
Luiz <bigodespy@gmail.com>
2016-03-03 00:22:47 -0800

    Postits working with parameters

---
+++ js/postit.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded", function(event)
+{
+  var title = getParameterByName('ttl');
+  var description = getParameterByName('desc');
+  var bgColor = "#"+ getParameterByName('bgc');
+  var textColor = "#"+ getParameterByName('txtc');
+
+  document.getElementById('elTitle').innerHTML = title == null ? "Invalid" : title;
+  document.getElementById('elDescription').innerHTML = description == null ? "Invalid" : description;
+
+  var element = document.getElementById('element');
+  element.style.color = textColor == null ? "#000" : textColor;
+  element.style.background = bgColor == null ? "#ffc" : bgColor;
+});
